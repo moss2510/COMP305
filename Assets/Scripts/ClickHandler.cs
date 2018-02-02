fca1a48775ef99578e2356a2b7574e1d7e69a396
moss2510 <31931251+moss2510@users.noreply.github.com>
2018-02-02 14:20:00 -0500

    week 2
    completed in lab

---
+++ Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+
+    //public int someNumber;
+    public GameObject Moss;
+    public Sprite newSprite;
+    public Sprite firstSprite;
+    public Sprite secondSprite;
+
+    private Rigidbody2D rbody;
+    private SpriteRenderer sRend;
+    
+
+    // Use this for initialization
+    void Start () {
+        //Debug.Log(someNumber);
+        rbody = Moss.GetComponent<Rigidbody2D>();
+        sRend = Moss.GetComponent<SpriteRenderer>();
+
+        rbody.velocity = new Vector2(2, 0);
+        
+	}
+
+    private void OnMouseDown()
+    {
+        //rbody = Moss.GetComponent<Rigidbody2D>();
+        rbody.velocity *= -1;
+        if(sRend.sprite == firstSprite)
+        {
+            sRend.sprite = secondSprite;
+        }
+        else
+        {
+            sRend.sprite = firstSprite;
+        }
+        
+
+    }
+
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        Rigidbody2D rbody = this.gameObject.GetComponent<Rigidbody2D>();
+        rbody.velocity = new Vector2(2, 0);
+
+	}
+	
+}
