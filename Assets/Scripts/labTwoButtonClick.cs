d370692672986cc855953a809bde76621deed18c
moss2510 <31931251+moss2510@users.noreply.github.com>
2018-02-04 16:06:15 -0500

    Lab2 color change
    Added the fuctions for on click change color

---
+++ Assets/Scripts/labTwoButtonClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class labTwoButtonClick : MonoBehaviour {
+
+    
+    public Rigidbody2D player;
+    public Sprite sp;
+
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    private void OnMouseDown()
+    {
+        SpriteRenderer sRend = player.GetComponent<SpriteRenderer>();
+        sRend.sprite = sp;
+
+
+    }
+
+}
